11700fb1175840af74916d510cd0b32131d2e5da
Gerald Gmainer <gerald.gmainer@designium.jp>
2024-07-13 23:07:57 +0200

    build docker image

--- budget-book-backend/build.gradle.kts
+++ budget-book-backend/build.gradle.kts
@@ -16,6 +16,10 @@ version = "0.0.1-SNAPSHOT"
 java {
     sourceCompatibility = JavaVersion.VERSION_21
     targetCompatibility = JavaVersion.VERSION_21
+
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
 }
 
 kotlin {
